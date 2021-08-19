eeb91c97f34112b32d1fded24266393db2d428f3
Piotr <p.skolorzynski@gmail.com>
2021-08-19 20:41:34 +0200

    lesson 6 - useState hook

--- react-tutorial-thenetninja/src/Home.js
+++ react-tutorial-thenetninja/src/Home.js
@@ -1,18 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
 
-    const handleClick = e => {
-        console.log('Hello, dudes', e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log(`Hello, ${name}`, e.target);
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+    const handleClick = () => {
+        setName('luigi');
+        setAge(30);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is {age} years old</p>
             <button onClick={handleClick}>Click me!</button>
-            <button onClick={e => handleClickAgain('Piotr', e)}>Click me again!</button>
         </div>
     );
 }
