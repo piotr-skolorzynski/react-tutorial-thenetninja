e263a3f888024e28c6e789d381632d8b77b29fed
Piotr <p.skolorzynski@gmail.com>
2021-08-19 18:50:21 +0200

    lesson 2 - dynamic values in templates

--- react-tutorial-thenetninja/src/App.js
+++ react-tutorial-thenetninja/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = "http://www.google.pl";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ 10 }</p>
+        <p>{ [1,2,3,4,5,6] }</p>
+        <p>{ Math.random() * 10 }</p>
+        <a href={ link } target="_blank">Google site</a>
       </div>
     </div>
   );
 }
 
 export default App;
+
+//dynamicznie nie da się dodać obiektu np. person = {name: "mario", age: 30} oraz booleans
