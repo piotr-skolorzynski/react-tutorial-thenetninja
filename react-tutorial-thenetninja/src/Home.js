5d38884d32490d9be756a91fe02579671da1f5f6
Piotr <p.skolorzynski@gmail.com>
2021-08-19 20:29:53 +0200

    lesson 5 - click events

--- react-tutorial-thenetninja/src/Home.js
+++ react-tutorial-thenetninja/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = e => {
+        console.log('Hello, dudes', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log(`Hello, ${name}`, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me!</button>
+            <button onClick={e => handleClickAgain('Piotr', e)}>Click me again!</button>
         </div>
     );
 }
