0200382e87e04f3a9816060a2be9383c3f0b0254
Piotr <p.skolorzynski@gmail.com>
2021-08-20 22:32:22 +0200

    lesson 18 - exact match routes

--- react-tutorial-thenetninja/src/App.js
+++ react-tutorial-thenetninja/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
 
@@ -10,10 +11,12 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
             </Route>
-
+            <Route path="/create">
+              <Create />
+            </Route>
           </Switch>
         </div>
       </div>
