a1bc2ad7613c3037d294e6530dc2ad5d70d80b90
Piotr <p.skolorzynski@gmail.com>
2021-08-20 22:52:17 +0200

    lesson 20 - useEffect Cleanup

--- react-tutorial-thenetninja/src/useFetch.js
+++ react-tutorial-thenetninja/src/useFetch.js
@@ -6,7 +6,9 @@ const useFetch = url => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+        
+        fetch(url, { signal: abortCont.signal })
             .then(response => {
                 if (!response.ok) {
                     throw Error('could not fetch the data for that resource')
@@ -19,9 +21,16 @@ const useFetch = url => {
                 setError(null);
             })
             .catch(err => {
-                setIsPending(false);
-                setError(err.message);
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                } else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
             })
+
+            return () => abortCont.abort();
+
     }, [url]);
 
     return { data, isPending, error };
