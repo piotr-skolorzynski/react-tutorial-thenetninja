d91a7e3fd00b325f9c355de736a7350c60a1c8de
Piotr <p.skolorzynski@gmail.com>
2021-08-23 23:39:23 +0200

    lesson 28 - 404 pages

--- react-tutorial-thenetninja/src/App.js
+++ react-tutorial-thenetninja/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDeatails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -20,6 +21,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*"> {/* co to znaczy? że złap każdy inny route*/}
+              <NotFound />
             </Route>
           </Switch>
         </div>
